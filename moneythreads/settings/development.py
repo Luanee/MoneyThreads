eb76818031aeb71341740371399a09dcddb8e064
Luanee <pit_nahrstedt@web.de>
2021-02-27 15:30:54 +0100

    MT-002: create project

--- moneythreads/settings/development.py
+++ moneythreads/settings/development.py
@@ -1,6 +1,6 @@
 from project_name.settings.production import *
 
-TMP_PATH = os.path.abspath(os.path.join(PROJECT_ROOT, 'tmp'))
+DB_PATH = os.path.abspath(os.path.join(PROJECT_ROOT, 'db'))
 
 DEBUG = TEMPLATE_DEBUG = True
 SECRET = '42'
@@ -8,7 +8,7 @@ SECRET = '42'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(TMP_PATH, 'db.sqlite3'),
+        'NAME': os.path.join(DB_PATH, 'db.sqlite3'),
     }
 }
 
